9e809a76bcc68cc606e99b86185f8f1277aee8de
jgooday48 <joelgooday48@gmail.com>
2024-01-09 08:11:02 +0000

    set up FE file structure and install packages

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom'
+  },
+   css: {
+    modules: false,
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/dist/css/bootstrap.min.css";`,
+      },
+    },
+  }
+})
