3d2a7c96d53ba3d6b7428275ba2315821a78f744
pockche123 <130081044+pockche123@users.noreply.github.com>
2024-01-12 11:08:34 +0000

    change made

--- src/pages/TouristGuidePage/index.jsx
+++ src/pages/TouristGuidePage/index.jsx
@@ -2,21 +2,28 @@ import React, { useState, useEffect} from 'react'
 
 import axios from 'axios';
 import { GuidesList } from '../../components';  // Adjust the import path based on your project structure
+import { useLocation } from 'react-router';
 
 const TouristGuidePage = () => {
   const [guides, setGuides] = useState([]);
+  const location = useLocation()
+  const filters = location.state && location.state.filters
 
-  useEffect(() => {
-    const fetchGuides = async () => {
-      try {
-        const { data } = await axios.get('http://localhost:5000/guides');  // Update the API endpoint
-        setGuides(data['all guides']);
-      } catch (error) {
-        console.error('Error fetching guides:', error);
-        setGuides([]); // Set guides to an empty array in case of an error
-      }
+
+     const fetchGuides = async () => {
+      // try {
+      //   const { data } = await axios.get('http://localhost:5000/guides');  // Update the API endpoint
+      //   setGuides(data['all guides']);
+      // } catch (error) {
+      //   console.error('Error fetching guides:', error);
+      //   setGuides([]); // Set guides to an empty array in case of an error
+      // }
     };
 
+
+  useEffect(() => {
+    console.log(filters)
+ 
     fetchGuides();
   }, []);
 
