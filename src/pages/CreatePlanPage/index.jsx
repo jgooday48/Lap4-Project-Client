fc0c8286dc2e442e12eb0c036ae87dbb7848532d
pockche123 <130081044+pockche123@users.noreply.github.com>
2024-01-15 14:33:41 +0000

    watched linked

--- src/pages/CreatePlanPage/index.jsx
+++ src/pages/CreatePlanPage/index.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
-import { useLocation, useParams } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate, useParams } from 'react-router'
 import './index.css'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import GuideActivities from '../IndividualGuidePage/GuideActivities';
 import AddActivitiesToPlan from './AddActivitiesToPlan';
+import { format } from 'date-fns';
+
+import axios from 'axios';
+import { baseApi } from '../../utils/baseApi';
+import { toast } from 'react-toastify';
 
 
 const CreatePlanPage = () => {
@@ -12,41 +16,97 @@ const CreatePlanPage = () => {
     const location = useLocation()
     const guide = location.state && location.state.guide
     const [notes, setNotes] = useState()
-    const [dateFrom, setDateFrom] = useState(null);
-    const [dateTo, setDateTo] = useState(null);
+    // const [dateFrom, setDateFrom] = useState(null);
+    // const [dateTo, setDateTo] = useState(null);
     const [activityIds, setActivityIds] = useState([])
 
+    const touristId = localStorage.getItem('touristId')
+    const navigate = useNavigate()
+    const [dateTimeFrom, setDateTimeFrom] = useState(null);
+    const [dateTimeTo, setDateTimeTo] = useState(null);
+
+    // ... (existing code)
+
+    const createPlan = async (e) => {
+        e.preventDefault()
+        const formattedDateTimeFrom = dateTimeFrom ? format(dateTimeFrom, 'yyyy-MM-dd HH:mm:ss') : null;
+        const formattedDateTimeTo = dateTimeTo ? format(dateTimeTo, 'yyyy-MM-dd HH:mm:ss') : null;
+
+        const body = {
+            "tourist_id": touristId,
+            "guide_id": guideId,
+            "date_to": formattedDateTimeTo.toString(),
+            "date_from": formattedDateTimeFrom.toString(),
+            "status": "ONGOING",
+            "notes": notes,
+            "activity_ids": activityIds,
+
+        };
+
+        await axios.post(baseApi + "plans", body)
+            .then(res => toast.success('New plan added to your "Plans"'))
+            .catch(e => {
+                console.log(e)
+                toast.error("Unable to create plan")
+
+            })
+
+
+    };
+
+    // useEffect(() => {
+    //     console.log("dateTo: ", dateTimeTo)
+    //     console.log("dateFrom: ", dateTimeFrom)
+    //     console.log("touristId: ", touristId)
+    //     console.log("activity_ids: ", activityIds)
+    //     console.log("notes: ", notes)
+    // }, )
 
 
 
 
     return (
         <div className="create-plan">
+
             <div className="plan-guide-info">
+                <button className="btn btn-secondary" onClick={() => navigate(-1)}>&larr; Go back</button>
                 <b>Create plans with {guide.name.substring(0, guide.name.indexOf(" "))}</b>
                 <img src={guide.images[0]} alt="guide-pic" />
             </div>
             <div className="create-plan-form-section">
-                <form>
+                <b>Plan Details</b>
+                <form onSubmit={createPlan} className="create-form">
 
                     <div>
-                        <label>Date from: </label>
+
+                        <label>Date and Time from: </label>
                         <DatePicker
-                            selected={dateFrom}
-                            onChange={(date) => setDateFrom(date)}
+                            selected={dateTimeFrom}
+                            onChange={(date) => setDateTimeFrom(date)}
+                            showTimeSelect
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            timeCaption="Time"
+                            minDate={new Date()}
+                            dateFormat="dd/MM/yyyy HH:mm"
                             className="form-control"
-                            dateFormat="dd/MM/yyyy"
                         />
                     </div>
                     <div>
-                        <label htmlFor="">Date to: </label>
+                        <label>Date and Time to: </label>
                         <DatePicker
-                            selected={dateTo}
-                            onChange={(date) => setDateTo(date)}
+                            selected={dateTimeTo}
+                            onChange={(date) => setDateTimeTo(date)}
+                            showTimeSelect
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            timeCaption="Time"
+                            minDate={dateTimeFrom}
+                            dateFormat="dd/MM/yyyy HH:mm"
                             className="form-control"
-                            dateFormat="dd/MM/yyyy"
                         />
                     </div>
+                    <label>Activities to include: </label>
                     <AddActivitiesToPlan id={guideId} setActivityIds={setActivityIds} activityIds={activityIds} />
                     <div>
                         <label>Notes:</label>
