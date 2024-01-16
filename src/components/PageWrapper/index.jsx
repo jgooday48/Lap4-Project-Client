4a22e66bd2ab1e067c5d52409ccf878d09e0e76d
Thomas Suggitt <tmjs2001@gmail.comgit config --global user.name TSuggitt>
2024-01-16 10:22:14 +0000

    logout added for guides and tourists

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -11,6 +11,25 @@ const PageWrapper = () => {
   const { guide, setGuide } = useGuide();
   const { welcome, setWelcome } = useWelcome();
 
+  const handleTouristLogout = () =>{
+    localStorage.removeItem("touristId")
+    localStorage.removeItem("touristUsername")
+    localStorage.removeItem("tourist_token")
+    localStorage.removeItem("tourist_refresh")
+    setTourist(false)
+    setWelcome(true)
+  }
+
+  const handleGuideLogout = () =>{
+    localStorage.removeItem("guide_id")
+    localStorage.removeItem("guide_Username")
+    localStorage.removeItem("guide_token")
+    localStorage.removeItem("guide_refresh")
+    setGuide(false)
+    setWelcome(true)
+  
+  }
+
   // const handleTouristLogin = () => {
   //   setTourist(true);
   //   setWelcome(false);
@@ -53,7 +72,7 @@ const PageWrapper = () => {
                     <NavLink to="/livechat">Chat</NavLink>
                     <NavLink to="/profile">Profile</NavLink>
                     <section className='logout-button'>
-                    <NavLink to="/logout">Logout</NavLink>
+                    <NavLink to="/" onClick={handleTouristLogout}>Logout</NavLink>
                     </section>
                   </section>
                 </nav>
@@ -66,18 +85,28 @@ const PageWrapper = () => {
 
 
       {guide && (
+        <>
         <div className='touristWrapper'>
-          <header>
-            <nav>
-              <section className='nav-link'>
+          <header className="tourist-header">
+          <span className='brand'>TravelGuide</span>
+          <div className="tourist-nav">
+            <nav id="link-navbar">
+              <section className='links'>
                 <NavLink to="/guidehomepage">Home</NavLink>
                 <NavLink to="/guideprofilepage">Your profile</NavLink>
+                <section className='logout-button'>
+                    <NavLink to="/" onClick={handleGuideLogout}>Logout</NavLink>
               </section>
+              </section>
+              
             </nav>
+            </div>
           </header>
 
-          <Outlet />
+          
         </div>
+        <Outlet />
+        </>
       )}
 
       <footer id="footer">
