56a00d50d65a9f0bed880d962ab31c2678122934
jgooday48 <joelgooday48@gmail.com>
2024-01-17 10:25:26 +0000

    add extra tests

--- src/pages/WebSocketPage/index.jsx
+++ src/pages/WebSocketPage/index.jsx
@@ -1,6 +1,6 @@
 import {React, useState, useEffect } from 'react'
 import ScrollToBottom from "react-scroll-to-bottom"
-import "./WebSocketCall.css"
+import "../../components/WebSocketCall/WebSocketCall.css"
 
 const WebSocketCall = ({ socket, username, room }) => {
   const [message, setMessage] = useState("");
